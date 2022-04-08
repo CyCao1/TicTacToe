099c2c786d1a9328df0d615c41147bf2c5205009
CyCao1 <90354416+CyCao1@users.noreply.github.com>
2022-04-08 04:50:39 -0400

    Delete TicTacToe & Other directory

--- TicTacToe & Other/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    /**
-     * This class has main() file, start the project from here
-     */
-    public static void main(String[] args) {
-        new GameCenter().start();
-    }
-}
